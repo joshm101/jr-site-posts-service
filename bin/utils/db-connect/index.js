479d0901963eec5e08babf7e6af4aef748a9d272
Joshua Mora <jshm101@gmail.com>
2019-02-25 22:05:58 -0800

    test environment setup

--- bin/utils/db-connect/index.js
+++ bin/utils/db-connect/index.js
@@ -1,14 +1,20 @@
 const mongoose = require('mongoose')
 
 const validDevEnv = require('../valid-dev-env')
+const validTestEnv = require('../valid-test-env')
 
 const env = process.env.NODE_ENV || 'development'
+
+console.log('env: ', env)
 
 let MONGODB_CONNECTION_URI = ''
 let MONGODB_CONNECTION_USERNAME = ''
 let MONGODB_CONNECTION_PASSWORD = ''
 
-const envValid = validDevEnv()
+const envValid = (
+  (validDevEnv() && env === 'development') ||
+  (validTestEnv() && env === 'test')
+)
 
 if (!envValid) {
   process.exit(1)
@@ -24,6 +30,18 @@ if (env === 'development' && envValid) {
   MONGODB_CONNECTION_URI = JR_SITE_DEV_DB_CONNECTION_URI
   MONGODB_CONNECTION_USERNAME = JR_SITE_DEV_DB_CONNECTION_USERNAME
   MONGODB_CONNECTION_PASSWORD = JR_SITE_DEV_DB_CONNECTION_PASSWORD
+}
+
+if (env === 'test' && envValid) {
+  const {
+    JR_SITE_TEST_DB_CONNECTION_URI,
+    JR_SITE_TEST_DB_CONNECTION_USERNAME,
+    JR_SITE_TEST_DB_CONNECTION_PASSWORD
+  } = process.env
+
+  MONGODB_CONNECTION_URI = JR_SITE_TEST_DB_CONNECTION_URI
+  MONGODB_CONNECTION_USERNAME = JR_SITE_TEST_DB_CONNECTION_USERNAME
+  MONGODB_CONNECTION_PASSWORD = JR_SITE_TEST_DB_CONNECTION_PASSWORD
 }
 
 const CONNECTION_STRING = (
