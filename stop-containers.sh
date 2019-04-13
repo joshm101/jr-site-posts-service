134356d3fa5611c7eaf72dd66969f3f7f3aa4303
Joshua Mora <jshm101@gmail.com>
2019-04-13 15:47:21 -0700

    configuration for dockerized dev environment

---
+++ stop-containers.sh
@@ -0,0 +1,4 @@
+docker kill jr-site-posts-service-dev-container
+docker rm jr-site-posts-service-dev-container
+docker kill jr-site-posts-service-prod-container
+docker rm jr-site-posts-service-prod-container
